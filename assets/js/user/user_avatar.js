17be7180cb32bf25f2624f4032f4f0e77b99477b
Muchjoy <292477110@qq.com>
2020-09-06 16:47:06 +0800

    更换头像

--- assets/js/user/user_avatar.js
+++ assets/js/user/user_avatar.js
@@ -1,4 +1,5 @@
 $(function () {
+    let layer = layui.layer
     //点击上传按钮  触发文件域
     $("#btnChooseImage").on('click', function () {
         //文件域点击事件
@@ -37,6 +38,35 @@ $(function () {
         $("#image").cropper('destroy').attr('src', imgUrl).cropper(options)
 
     });
+
+    $("#btnUpload").on('click', function () {
+        // 1. 要拿到用户裁剪之后的头像
+        var dataURL = $("#image")
+            .cropper('getCroppedCanvas', {
+                // 创建一个 Canvas 画布
+                width: 100,
+                height: 100
+            })
+            .toDataURL('image/png') // 将 Canvas 画布上的内容，转化为 base64 格式的字符串
+
+        $.ajax({
+            type: 'POST',
+            url: '/my/update/avatar',
+            headers: {
+                Authorization: localStorage.getItem('token')
+            },
+            data: {
+                avatar: dataURL
+            },
+            success: function (res) {
+                if (res.status !== 0) return layer.msg("更换头像失败")
+                layer.msg("更换头像成功")
+                //重新渲染
+                window.parent.getData()
+            }
+        })
+
+    })
 
 
 })
